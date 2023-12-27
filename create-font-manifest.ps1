51bb62607a96d06b07e43f97e78c363cb6566876
FlawlessCasual17 <07e5297d5b@c0x0.com>
2023-12-26 17:25:22 -0700

    Add `transwiz` version 1.19
    Signed-off-by: FlawlessCasual17 <07e5297d5b@c0x0.com>

--- create-font-manifest.ps1
+++ create-font-manifest.ps1
@@ -2,4 +2,4 @@ $name = $args[0]
 if ($args.Count -eq 2) {
     Write-Host 'Please use only one argument'; Break
 }
-Copy-Item "$PSScriptRoot\..\font-manifest-template.json" "$PSScriptRoot\..\bucket\$name.json" -Force
+Copy-Item "$PSScriptRoot\font-manifest-template.json" "$PSScriptRoot\bucket\$name.json" -Force
